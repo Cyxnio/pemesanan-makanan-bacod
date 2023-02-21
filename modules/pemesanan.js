da019da5826e01bdb788c4bad26b42d30a6b599e
yesssr <adimuhammadyasir93@gmail.com>
2023-02-21 23:00:40 +0700

    update finish pemesanan

--- modules/pemesanan.js
+++ modules/pemesanan.js
@@ -1,12 +1,13 @@
-app.get("/pesan", (req, res) => {
+app.post("/pesan", (req, res) => {
   let data = {
-    no_meja: req.body.no_meja,
-    tanggal_pemesan: req.body.tanggal_pemesan,
-    nama_pemesan: req.body.nama_pemesan,
+    nomor_meja: req.body.nomor_meja,
+    tanggal_pesan: req.body.tanggal_pesan,
     pesanan: req.body.pesanan,
-    jumlah: req.body.jumlah
+    nama_pemesan: req.body.nama_pemesan,
+    total_pesanan: req.body.total_pesanan,
+    no_telepon: req.body.no_telepon,
+    note: req.body.note
   }
-  data.pesanan = '[{ "nomer": 1, "nama": "nasi goreng", "harga": 15000, "jumlah": 2 }, { "nomer" : 2, "nama": "soto", "harga": 15000, "jumlah": 3 }]';
 
   let parsed = JSON.parse(data.pesanan);
   parsed.forEach(items => {
@@ -15,26 +16,31 @@ app.get("/pesan", (req, res) => {
     connection.query("select nomer from sql12599979.tabel_menu where nomer = ? and stok_makanan = 1", items.nomer, (error, result) => {
     console.log(stokAvailable);
       if (error) {
-        // res.status(200).json({
-        // success: false,
-        // results: "menu yang anda pesan kosong"
-        // });
+        res.status(200).json({
+        success: false,
+        results: "menu yang anda pesan kosong"
+        });
         stokAvailable = false;
         console.log(error);
       } else {
-        stokAvailable = true;
-        console.log(result);
+        connection.query(`insert into sql12599979.tabel_pemesanan set ?`, data, (errorInsert, resultInsert) => {
+          if(errorInsert){
+            res.status(500).json({
+              success: false,
+              results: errorInsert
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            results: resultInsert
+          });
+        }
+      });
       }
     })
     console.log(stokAvailable);
     if(stokAvailable) {
-      connection.query(`insert into sql12599979.tabel_pemesanan set ?`, data, (error, result) => {
-        if(error){
-          console.log(error);
-      } else {
-        console.log(result);
-      }
-    });
+
   } else {
     console.log("stok kosong");
   }
